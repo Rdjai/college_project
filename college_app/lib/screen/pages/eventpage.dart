d986e032929a6e36faec827591803ed82bf05cc2
Jagannath kashyap <jagannathkashyap38@gmail.com>
2024-05-30 12:28:11 +0530

    student find working properly '

--- college_app/lib/screen/pages/eventpage.dart
+++ college_app/lib/screen/pages/eventpage.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:college_app/modal/Professor_profile_modal.dart';
 import 'package:college_app/screen/home/student.dart';
 import 'package:college_app/screen/pages/studentList.dart';
+import 'package:college_app/widgets/dob.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -9,13 +15,74 @@ class EventPage extends StatefulWidget {
   State<EventPage> createState() => _EventPageState();
 }
 
+TextEditingController _eventtitlecontrol = TextEditingController();
+TextEditingController _eventdescriptioncontrol = TextEditingController();
+TextEditingController _eventdepartmentcontrol = TextEditingController();
+TextEditingController _eventsemestercontrol = TextEditingController();
+var eventdate;
+
 class _EventPageState extends State<EventPage> {
+  Future<void> callevntApi(BuildContext context) async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? token = prefs.getString('token');
+
+      if (token == null) {
+        throw Exception('No token found');
+      }
+
+      // Validate input fields
+      if (_eventtitlecontrol.text.isEmpty ||
+          _eventdescriptioncontrol.text.length < 50 ||
+          _eventdepartmentcontrol.text.isEmpty ||
+          _eventsemestercontrol.text.isEmpty) {
+        throw Exception(
+            'All fields are required and description must be at least 50 characters long.');
+      }
+
+      final res = await http.post(
+        Uri.parse('http://localhost:3000/api/v1/admin/create/event'),
+        body: jsonEncode({
+          "title": _eventtitlecontrol.text,
+          "description": _eventdescriptioncontrol.text,
+          "department": {"name": _eventdepartmentcontrol.text},
+          "semester": {"name": _eventsemestercontrol.text},
+          "event_date": eventdate
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      debugPrint('Response status: ${res.statusCode}');
+      final Map<String, dynamic> response = jsonDecode(res.body);
+      debugPrint('Response body: $response');
+
+      if (res.statusCode == 200) {
+        // Successfully created event
+        if (mounted) {
+          Navigator.pop(context); // Close the dialog
+        }
+      } else {
+        // Show error message
+        if (mounted) {
+          _showErrorDialog(context, response['message']);
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        _showErrorDialog(context, e.toString());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 12,
           ),
           Row(
@@ -37,19 +104,20 @@ class _EventPageState extends State<EventPage> {
               )
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 12,
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
-                children: [partyDetails()],
+              Expanded(
+                child: partyDetails(),
               ),
-              Column(
-                children: [khatanahaikarale()],
-              )
+              Expanded(
+                child: _EventPushPage(),
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
@@ -61,11 +129,11 @@ class _EventPageState extends State<EventPage> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Card(
-            margin: EdgeInsets.all(17),
+            margin: const EdgeInsets.all(17),
             child: Container(
               height: MediaQuery.of(context).size.height / 6,
               width: MediaQuery.of(context).size.height / 8,
-              child: Column(
+              child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -80,10 +148,10 @@ class _EventPageState extends State<EventPage> {
           ),
           Card(
             child: Container(
-              padding: EdgeInsets.all(25),
+              padding: const EdgeInsets.all(25),
               height: MediaQuery.of(context).size.height / 6,
               width: MediaQuery.of(context).size.height / 2,
-              child: Column(
+              child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -130,6 +198,75 @@ class _EventPageState extends State<EventPage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget _EventPushPage() {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              _buildTextField("title", "Enter event title", _eventtitlecontrol),
+              _buildTextField("description", "Enter event description",
+                  _eventdescriptioncontrol),
+              _buildTextField(
+                  "department", "Enter department", _eventdepartmentcontrol),
+              _buildTextField(
+                  "semester", "Enter semester", _eventsemestercontrol),
+              DobPicker(
+                  onDateSelected: (p0) {
+                    setState(() {
+                      eventdate = p0.toIso8601String();
+                    });
+                  },
+                  text: "select date"),
+              ElevatedButton(
+                  onPressed: () {
+                    callevntApi(context);
+                  },
+                  child: Text("Create Event"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('College Management System Notification'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildTextField(
+      String label, String hint, TextEditingController textControl) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: TextFormField(
+        controller: textControl,
+        decoration: InputDecoration(
+          labelText: label,
+          hintText: hint,
+          border: const OutlineInputBorder(),
+        ),
       ),
     );
   }
--- college_app/lib/view/studentfind.dart
+++ college_app/lib/view/studentfind.dart
@@ -1,31 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-
-class Student {
-  final int id;
-  final String name;
-  final String designation;
-  final String image;
-
-  Student({
-    required this.id,
-    required this.name,
-    required this.designation,
-    required this.image,
-  });
-
-  factory Student.fromJson(Map<String, dynamic> json) {
-    return Student(
-      id: json['id'],
-      name: json['name'],
-      designation: json['designation'],
-      image: json['image'],
-    );
-  }
-}
 
 class StudentFindScreen extends StatefulWidget {
   const StudentFindScreen({Key? key}) : super(key: key);
@@ -38,57 +14,66 @@ class _StudentFindScreenState extends State<StudentFindScreen> {
   List<Student> _allStudents = [];
   List<Student> _foundStudents = [];
 
-  Future<void> _fetchAllStudents() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? token = prefs.getString('token');
-    if (token == null) {
-      throw Exception('No token found');
-    }
-    final response = await http.get(
-      Uri.parse('http://localhost:3000/api/v1/admin/get/allStudents'),
-      headers: {
-        'Authorization': 'Bearer $token',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final jsonResponse = json.decode(response.body);
-      if (jsonResponse.containsKey('students') &&
-          jsonResponse['students'] != null) {
-        List<dynamic> studentsData = jsonResponse['students'];
-        _allStudents =
-            studentsData.map((data) => Student.fromJson(data)).toList();
-        setState(() {
-          _foundStudents = _allStudents;
-        });
-      } else {
-        throw Exception('No student data found');
-      }
-    } else {
-      throw Exception('Failed to load students: ${response.statusCode}');
-    }
-  }
-
-  void _runFilter(String enteredKeyword) {
-    if (enteredKeyword.isEmpty) {
-      setState(() {
-        _foundStudents = _allStudents;
-      });
-    } else {
-      setState(() {
-        _foundStudents = _allStudents
-            .where((student) => student.name
-                .toLowerCase()
-                .contains(enteredKeyword.toLowerCase()))
-            .toList();
-      });
-    }
-  }
-
   @override
   void initState() {
     super.initState();
     _fetchAllStudents();
+  }
+
+  Future<void> _fetchAllStudents() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? token = prefs.getString('token');
+      if (token == null) {
+        throw Exception('No token found');
+      }
+      final response = await http.get(
+        Uri.parse('http://localhost:3000/api/v1/admin/get/allStudents'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        print('Response JSON: $jsonResponse'); // Debug print
+
+        if (jsonResponse.containsKey('students') &&
+            jsonResponse['students'] != null) {
+          List<dynamic> studentsData = jsonResponse['students'];
+          print('Students Data: $studentsData'); // Debug print
+          setState(() {
+            _allStudents =
+                studentsData.map((data) => Student.fromJson(data)).toList();
+            _foundStudents = _allStudents;
+          });
+          print(_foundStudents);
+        } else {
+          throw Exception('No student data found');
+        }
+      } else {
+        throw Exception('Failed to load students: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching students: $e'); // Debug print
+      _showErrorDialog('Failed to load students.');
+    }
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Student> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _allStudents;
+    } else {
+      results = _allStudents
+          .where((student) =>
+              student.name.toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundStudents = results;
+    });
   }
 
   @override
@@ -96,7 +81,7 @@ class _StudentFindScreenState extends State<StudentFindScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Student List'),
+        title: const Text('User List'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
@@ -107,7 +92,7 @@ class _StudentFindScreenState extends State<StudentFindScreen> {
               decoration: InputDecoration(
                 contentPadding:
                     const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15),
-                hintText: 'Search',
+                hintText: "Search",
                 suffixIcon: const Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20.0),
@@ -115,7 +100,9 @@ class _StudentFindScreenState extends State<StudentFindScreen> {
                 ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(
+              height: 20,
+            ),
             Expanded(
               child: _foundStudents.isNotEmpty
                   ? ListView.builder(
@@ -131,20 +118,62 @@ class _StudentFindScreenState extends State<StudentFindScreen> {
                             backgroundColor: Colors.transparent,
                           ),
                           title: Text(_foundStudents[index].name),
-                          subtitle: Text(_foundStudents[index].designation),
+                          subtitle:
+                              Text('${_foundStudents[index].designation}'),
                         ),
                       ),
                     )
-                  : const Center(
-                      child: Text(
-                        'No results found. Please try a different search.',
-                        style: TextStyle(fontSize: 18),
-                      ),
+                  : const Text(
+                      'No results found. Please try a different search.',
+                      style: TextStyle(fontSize: 24),
                     ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('College Management System Notification'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Student {
+  final String id;
+  final String name;
+  final String designation;
+  final String image;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.designation,
+    required this.image,
+  });
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+      id: json['_id'],
+      name: "${json['firstName']} ${json['lastName']}",
+      designation: json['department']['name'] ?? 'No designation',
+      image: json['documents']['pic'],
     );
   }
 }
